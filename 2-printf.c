fcdcd2188c7af2f3eb646aff7789464f160d285c
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-05-28 17:38:16 +0400

    a function that produces output according to a format 2-printf.c 28/25/2023-16:37pm

--- 2-printf.c
+++ 2-printf.c
@@ -1,62 +1,170 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
 
-/**
- *@_printf:prints strings in binary form
- *
- *@format: the string of char
- *
- * Return: 0
- */
-void print_binary(unsigned int value)
-{
-	    if (value > 1)
-		            print_binary(value / 2);
 
-	        putchar('0' + (value % 2));
+/* for printing unsigned integers */
+
+/**
+ * un_num - prints unsigned numbers
+ * @n: list of arguments
+ * @pbuff: hosts arrays
+ * @flags: checks flags
+ * @width: checks width
+ * @precision: checks precision
+ * @size: checks size
+ * Return: Printed characters
+ *
+ * Author: Magdalinr N
+ */
+
+int un_num(va_list n, char pbuff[], int flags, int width, int precision, int size)
+{
+	int i;
+	i = SIZE_B - 2;
+
+	unsigned long int num = va_arg(n, unsigned long int);
+
+	if (num == 0)
+		pbuff[i--] = '0';
+
+	pbuff[SIZE_B - 1] = '\0';
+
+	while (num > 0)
+	{
+		pbuff[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+	i++;
+
+	return (write_un(0, i, pbuff, flags, width, precision, size);)
 }
 
-int my_printf(const char *format, ...)
+/* octaal unsigned numbers printed */
+
+/**
+ * prtOCTAL - prints octal
+ * @n: number of arguments
+ * @pbuff: carries arrays
+ * @flags: checks for flags
+ * @width: checks width
+ * @precision: checks precision
+ * @size: checks size
+ * Return: Printed characters
+ *
+ * Author: Magdaline N
+ */
+
+int prtOCTAL(va_list n, char pbuff[], int flags, int width, int precision, int size)
 {
-	int chars_printed = 0;
-	/* to keep track of the character count*/
-	char b;
-	/*the storage of the character to be processed*/
-	unsigned int binary_value;
-	/*to store the binary conversion*/
+	int i;
+	i = SIZE_B - 2;
 
-	va_list list;
+	unsigned long int num = va_arg(n, unsigned long int);
+	unsigned long int num1 = num;
 
-	va_start(list, format);
+	UNUSED(width);
 
-	while ((b = *format++))
+	num = new_unSize(num, size);
+
+	if (num == 0)
+		pbuff[i--] = '0';
+	pbuff[SIZE_B - 1] = '\0';
+
+	while (num > 0)
 	{
-		if (b == '%')
-		{
-			switch (*format++)
-			{
-				case 'b':
-					binary_value = va_arg(list, unsigned int);
-					printf("%b", binary_value);
-					chars_printed += sizeof(unsigned int) * 8;
-					/*coverts bytes to bits*/
-					break;
-				case '\0':
-					return (chars_printed);
-				default:
-					putchar(b);
-					chars_printed++;
-					break;
-			}
-		}
-		else
-		{
-			putchar(b);
-			chars_printed++;
-		}
+		pbuff[i--] = (num % 8) + '0';
+		num /= 8;
 	}
-	va_end(list);
 
-	return (chars_printed);
+	if (flags & F_HASH && num1 != 0){
+		pbuff[i--] = '0';
+	i++;
+
+	return (write_un(0, i, pbuff, flags, width, precision, size));
+}
+
+/* print haxadecimal number lower case a-f */
+
+/**
+ * hexa_lower - prints lower hexa decila values
+ * @n: arrays of numbers
+ * @pbuff: holds hexa values
+ * @flags: chacks flags
+ * @width: chacks width
+ * @precision: checks precision
+ * @size: checks size
+ * Return: Printed values in lower hexa
+ *
+ * Authror - Magdaline N
+ */
+int hexa_lower(va_list n, char pbuff[], int flags, 'x', width, precision, size)
+{
+	return (_hexa(n, "0123456789abcdef", pbuff, flags, 'x',
+				width, precision, size));
+}
+
+/* print HEXADECIMAL in UPPER */
+
+/**
+ * hexa_upper - prints hexa in UPPER
+ * @n: the array of numbers
+ * @pbuff: holds the values
+ * @flags: checks for flags
+ * @width: checks for width
+ * @precision: checks for precision
+ * @size: checks for size
+ * Return: values printed
+ */
+int hexa_upper(va_list n, char pbuff[], int flags, 'X', int width, 
+		int precision, int size)
+{
+	return (_hexa(n, "0123456789ABCDEF", pbuff, 
+				flags, 'X', width, precision, size));
+}
+
+/* prints hexa in both lower and UPPER */
+
+/**
+ * print_hexa - prints both upper and lower hexa values
+ * @n: array
+ * @map_to: where to place number
+ * @pbuff: to hold values
+ * @flags: checks for flags
+ * @flags1: checks again for flags
+ * @width: checks the width
+ * @precision: checks the precision
+ * @size: checks the size
+ * @size: checks the size
+ * Return: printed values 
+ */
+
+int print_hexa(va_list n, char map_to[], char pbuff[], 
+		int flags, char flags1, int width, int precision, int size)
+{
+	int i = SIZE_B - 2;
+	unsigned long int num = va_arg(n, unsigned long int);
+	unsigned long int num1 = num;
+
+	UNUSED(width);
+
+	num = new_unsize(num, size);
+
+	if (num == 0)
+		buffer[i--] = '0';
+
+	pbuff[SIZE_B - 1] = '\0';
+	
+	while (num > 0)
+	{
+		pbuff[i--] = map_to[num % 16];
+		num /= 16;
+	}
+
+	if (flags & F_HASH && num1 != 0)
+	{
+		pbuff[i--] = flags1;
+		pbuff[i--] = '0';
+	}
+	i++;
+
+	return (write_un(0, i, pbuff, flags, width, precision, size));
 }
