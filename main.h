a4d81d3fa8a9ee9e20dbac3958d133f16048b8cf
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-05-28 07:48:07 +0400

    changed main.h

--- main.h
+++ main.h
@@ -2,5 +2,8 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
+int my_printf(const char *format, ...);
+int my1_printf(const char *format, ...);
+void print_binary(unsigned int value);
 
 #endif
