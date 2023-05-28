4b29a1e912e6f0975a51a7d5a4fa590147fdd499
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-05-28 07:57:22 +0400

    changes to main.h -hot fix 1

--- main.h
+++ main.h
@@ -2,8 +2,9 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
-int my_printf(const char *format, ...);
-int my1_printf(const char *format, ...);
 void print_binary(unsigned int value);
+int my1_printf(const char *format, ...);
+int my_printf(const char *format, ...);
+int my2_printf(const char *format, ...);
 
 #endif
