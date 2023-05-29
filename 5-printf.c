4cdc8c9c23d91dca92d51116dbca9f8db7fd7a73
JenMora <jeniffermourah73@gmail.com>
2023-05-29 06:53:26 +0000

    flags about size code

---
+++ 5-printf.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*get_size - Calculates the size to cast the argument
+*@format: Formatted string in which to print the arguments
+*@i:arguments lists to be printed.
+*Return: Precision.
+*/
+int get_size(const char *format, int *i)
+{
+int a = *i + 1;
+int size = 0;
+if (format[a] == 'l')
+size = S_LONG;
+else if (format[a] == 'h')
+size = S_SHORT;
+
+if (size == 0)
+*i = a - 1;
+else
+*i = a;
+return (size);
+
+}
