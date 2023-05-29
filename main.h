5a5cd81748bced0c3bafe6c5b552a6b17ca7d65e
JenMora <128831350+JenMora@users.noreply.github.com>
2023-05-29 07:24:42 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,10 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
 
-int _printf(const char *format, ...);
-void print_binary(unsigned int value);
-int my1_printf(const char *format, ...);
-int my_printf(const char *format, ...);
-int my2_printf(const char *format, ...);
-
-#endif
