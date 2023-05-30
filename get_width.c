bedc6e90c9a74e10f91ee31b08b0ce0962f4578f
Magdaline Njuguna <128807862+MagzShiku@users.noreply.github.com>
2023-05-30 18:33:42 +0400

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+/* Authors - Jeniffer Moraa & Magdaline Njuguna */
 /**
 *get_width - Calculates the width for printing
 *@format: Formatted string in which to print the arguments
